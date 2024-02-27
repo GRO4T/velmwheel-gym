aa068a13189f037fc618ccc008f8b93d7c06b4f3
Damian Kolaska <damian.kolaska99@gmail.com>
2024-02-27 19:37:37 +0100

    chore: improve logging format

--- velmwheel_gym/logger.py
+++ velmwheel_gym/logger.py
@@ -4,5 +4,5 @@ import logging
 def init_logging(
     level: int = logging.DEBUG, filename: str = "./logs/velmwheel/default.log"
 ):
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    format = "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
     logging.basicConfig(level=level, filename=filename, format=format)
