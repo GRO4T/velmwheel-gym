4a22be7afccff8b436c3da845f2ab704c4fdd832
Damian Kolaska <damian.kolaska99@gmail.com>
2024-03-17 16:50:42 +0100

    chore: rotate logs after 20MB

--- velmwheel_gym/logger.py
+++ velmwheel_gym/logger.py
@@ -1,5 +1,6 @@
 import logging
 from functools import partial, partialmethod
+from logging.handlers import RotatingFileHandler
 
 # Add TRACE log level
 logging.TRACE = 5
@@ -18,6 +19,16 @@ def init_logging(level: str = "debug", filename: str = "./logs/velmwheel/default
             level = logging.TRACE
         case _:
             raise ValueError(f"Unknown log level: {level}")
+
     # pylint: disable=redefined-builtin
     format = "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
-    logging.basicConfig(level=level, filename=filename, format=format)
+
+    handler = RotatingFileHandler(filename, maxBytes=20 * 1024 * 1024, backupCount=5)
+    handler.setLevel(level)
+
+    formatter = logging.Formatter(format)
+    handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+    root_logger.addHandler(handler)
