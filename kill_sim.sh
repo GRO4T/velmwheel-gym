c4bef3891045a4d5121c5fb06f82c19fff9e08d9
Damian Kolaska <damian.kolaska99@gmail.com>
2024-04-04 20:04:52 +0200

    chore: do not kill tensorboard and vim in kill_sim.sh

--- kill_sim.sh
+++ kill_sim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -ef | grep -E "(velmwheel|gzserver|gzclient|nav2)" | grep -v grep | awk '{ print $2 }' | xargs -I{} kill -9 {}
+ps -ef | grep -E "(velmwheel|gzserver|gzclient|nav2)" | grep -vE "(grep|tensorboard|vim)" | awk '{ print $2 }' | xargs -I{} kill -9 {}
