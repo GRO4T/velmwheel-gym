f4a3de4f7c952505aab5476bc40fbb15a1845c6f
Damian Kolaska <damian.kolaska99@gmail.com>
2024-04-21 16:09:18 +0200

    fix: properly clamp lidar rays

--- velmwheel_gym/two_dim_env/lidar_2d.py
+++ velmwheel_gym/two_dim_env/lidar_2d.py
@@ -1,5 +1,4 @@
 import numpy as np
-from line_profiler import profile
 
 from .utils import *
 
@@ -10,20 +9,31 @@ class Lidar2D:
         self._max_range = max_range
         self._alphas = np.linspace(-np.deg2rad(fov) / 2, np.deg2rad(fov) / 2, rays)
 
-    @profile
     def scan(self) -> list:
         xcs = self._robot.env.xcs
         ycs = self._robot.env.ycs
         rcs = self._robot.env.rcs
 
         r = self._max_range
-        self._robot.xls = []
-        self._robot.yls = []
-        touches = []
+        self._robot.xls = np.array([])
+        self._robot.yls = np.array([])
         for alpha in self._alphas:
-            self._robot.xls.append(self._robot.xr + r * np.cos(alpha + self._robot.thr))
-            self._robot.yls.append(self._robot.yr + r * np.sin(alpha + self._robot.thr))
+            self._robot.xls = np.append(
+                self._robot.xls, self._robot.xr + r * np.cos(alpha + self._robot.thr)
+            )
+            self._robot.yls = np.append(
+                self._robot.yls, self._robot.yr + r * np.sin(alpha + self._robot.thr)
+            )
 
+        # Clamp the lidar readings to the environment boundaries
+        for i in range(len(self._robot.xls)):
+            self._robot.xls[i], self._robot.yls[i] = (
+                self._clamp_to_environment_boundaries(
+                    self._robot.xls[i], self._robot.yls[i]
+                )
+            )
+
+        touches = []
         for i, (xl, yl, alpha) in enumerate(
             zip(self._robot.xls, self._robot.yls, self._alphas)
         ):
@@ -51,8 +61,44 @@ class Lidar2D:
             if x >= 5 or x <= -5 or y >= 5 or y <= -5:
                 touches[i] = True
 
-        xls = [max(min(x, 5.0), -5) for x in self._robot.xls]
-        self._robot.xls = np.array(xls)
-        yls = [max(min(x, 5.0), -5) for x in self._robot.yls]
-        self._robot.yls = np.array(yls)
         return touches
+
+    def _clamp_to_environment_boundaries(self, x, y):
+        top_bound = ((5, 5), (-5, 5))
+        bottom_bound = ((-5, -5), (5, -5))
+        left_bound = ((-5, 5), (-5, -5))
+        right_bound = ((5, -5), (5, 5))
+
+        if x > 5:
+            if x > abs(y):
+                return line_intersection(
+                    right_bound, ((self._robot.xr, self._robot.yr), (x, y))
+                )
+            if y > 5:
+                return line_intersection(
+                    top_bound, ((self._robot.xr, self._robot.yr), (x, y))
+                )
+            return line_intersection(
+                bottom_bound, ((self._robot.xr, self._robot.yr), (x, y))
+            )
+        if x < -5:
+            if abs(x) > abs(y):
+                return line_intersection(
+                    left_bound, ((self._robot.xr, self._robot.yr), (x, y))
+                )
+            if y > 5:
+                return line_intersection(
+                    top_bound, ((self._robot.xr, self._robot.yr), (x, y))
+                )
+            return line_intersection(
+                bottom_bound, ((self._robot.xr, self._robot.yr), (x, y))
+            )
+        if y > 5:
+            return line_intersection(
+                top_bound, ((self._robot.xr, self._robot.yr), (x, y))
+            )
+        if y < -5:
+            return line_intersection(
+                bottom_bound, ((self._robot.xr, self._robot.yr), (x, y))
+            )
+        return x, y
--- velmwheel_gym/two_dim_env/utils.py
+++ velmwheel_gym/two_dim_env/utils.py
@@ -69,3 +69,20 @@ def clip_angle(th):
         return th
     except:
         raise Exception("th angle was not a number")
+
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return None
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
