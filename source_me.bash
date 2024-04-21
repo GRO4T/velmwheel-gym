8e322e77b978d727d2cf0b2c9b568ccf2b10fbaa
Damian Kolaska <damian.kolaska99@gmail.com>
2024-04-21 13:27:07 +0200

    chore: install tf-transformations in source_me.bash

--- source_me.bash
+++ source_me.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install ros-humble-tf-transformations
 source venv/bin/activate
 source "$VELMWHEEL_ROOT/source_me.bash"
 source "$VELMWHEEL_ROOT/install/velmwheel_gazebo_msgs/share/velmwheel_gazebo_msgs/local_setup.bash"
